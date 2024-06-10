c1a69eb6092a1ea6e32755a37545e65cf4589b53
Dixi <20200201@ricaldone.edu.sv>
2024-06-09 21:56:20 -0600

    1.0.2 se me olvido la base de datos

---
+++ Tickets.sql
@@ -0,0 +1,16 @@
+CREATE TABLE tbUsuarios(
+UUID_usuario VARCHAR2(50),
+Nombre VARCHAR2(50),
+Correo VARCHAR2(50),
+Contrasena VARCHAR2(50)
+);
+
+CREATE TABLE Ticket(
+UUID_ticket VARCHAR2(50),
+Descripcion VARCHAR2(250),
+Autor VARCHAR2(50),
+Email VARCHAR2(50),
+Fecha_Creacion DATE,
+Estado VARCHAR2(15),
+Fecha_FInalizacion DATE
+);
