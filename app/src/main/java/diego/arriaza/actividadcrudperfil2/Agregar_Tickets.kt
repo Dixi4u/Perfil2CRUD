37cdf2a3949d72a7713219f06e7980d32eaebb5b
Monterrosa <20230197@ricaldone.edu.sv>
2024-06-10 12:02:50 -0600

    vç1.0.4

---
+++ app/src/main/java/diego/arriaza/actividadcrudperfil2/Agregar_Tickets.kt
@@ -0,0 +1,20 @@
+package diego.arriaza.actividadcrudperfil2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Agregar_Tickets : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_agregar_tickets)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
