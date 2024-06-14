b19b15c4f938bdedc2d224f09acd5fdc0b740a9e
Dixi <20200201@ricaldone.edu.sv>
2024-06-13 21:32:35 -0600

    v1.0.7 funciona(en teoria(espero(ruego(necesito(imploro)))))

--- Tickets.sql
+++ Tickets.sql
@@ -11,7 +11,13 @@ Titulo VARCHAR2(50),
 Descripcion VARCHAR2(250),
 Autor VARCHAR2(50),
 Email VARCHAR2(50),
-Fecha_Creacion DATE,
+Fecha_Creacion VARCHAR(20),
 Estado VARCHAR2(15),
-Fecha_FInalizacion DATE
+Fecha_FInalizacion VARCHAR(20)
 );
+
+SELECT * FROM tbUsuarios WHERE Correo = 'a' AND Contrasena = 'a'
+
+select * from tbUsuarios
+
+Insert into tbUsuarios (UUID_usuario, Nombre, Correo, Contrasena) values ('a','a','a','a')
